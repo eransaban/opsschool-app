9875a157f6f4b33291878c8cb81a0e18a516a2b5
eransaban <54227507+eransaban@users.noreply.github.com>
2020-04-22 08:00:05 +0300

    add varibales to user and pass

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 # Setting up MariaDB
 app.config["MYSQL_HOST"] = 'mysql'
-app.config["MYSQL_USER"] = 'eks'
-app.config["MYSQL_PASSWORD"] = '102030'
+app.config["MYSQL_USER"] = '$dbuser'
+app.config["MYSQL_PASSWORD"] = '$dbpass'
 app.config["MYSQL_DB"] = 'prac'
 app.config["MYSQL_CURSORCLASS"] = 'DictCursor'
 # Initialising MariaDB
